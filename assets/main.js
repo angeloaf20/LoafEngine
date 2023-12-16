0b6000c6bafcbb2d3ca4a825b3ecaaffdd8c0a01
angeloaf20 <angelofoster01@gmail.com>
2023-12-16 12:18:01 -0500

    Updates

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,8 @@
-import { LoafRender } from './Core/LoafRender.js';
+import { LoafRender } from './core/LoafRender.js';
+import { ToolbarAdd } from './ui/Toolbar.Add.js';
 
 function main() {
+  const renderContainer = document.querySelector("#render-container");
   const loaf = new LoafRender();
   loaf.load();
 
@@ -10,8 +12,26 @@ function main() {
     requestAnimationFrame(animate);
   }
 
+  function handleResize() {
+    const width = renderContainer.innerWidth;
+    const height = renderContainer.innerHeight;
+
+    loaf.renderer.setSize(width, height);
+    loaf.camera.aspect = width / height;
+    loaf.camera.updateProjectionMatrix();
+  }
+
+  
+  // Attach handleResize to the window's resize event
+  window.addEventListener('resize', handleResize);
+
   // Start the animation loop
   animate();
+
+  // Add cube to toolbar
+  const addDropdown = document.getElementById("add-dropdown");
+  const boxAdd = new ToolbarAdd(addDropdown, "Box", loaf.scene_);
+  const sphereAdd = new ToolbarAdd(addDropdown, "Sphere", loaf.scene_);
 }
 
 main();
